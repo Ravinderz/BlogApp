152f079580a7508958090bcf8ff3c9ce49746512
Ravinderz <ravinderz@outlook.com>
2017-05-29 16:33:54 +0530

    added nav in index.html

--- app/app.js
+++ app/app.js
@@ -21,4 +21,136 @@ config(['$routeProvider', function($routeProvider) {
 	}).otherwise({
 		redirectTo: '/home'
 	});
+}]).
+controller('appCtrl',['$rootScope','$scope','$http','$location',function($rootScope,$scope,$http,$location,loginService){
+
+		var addr = $location.absUrl().split('/');
+		$rootScope.isLogged = false;
+		$rootScope.datenow = Date.now();
+		$rootScope.webAddr = addr[0]+"//"+addr[1]+addr[2];
+		$rootScope.user = {};
+
+		
+	 if(sessionStorage.getItem("user")){
+		 $rootScope.isLogged = true;
+		 $rootScope.user = angular.fromJson(sessionStorage.getItem("user"));
+	 }
+		
+
+}]).controller('loginCtrl',['$scope','$http','$uibModal',function($scope,$http,$uibModal){
+
+	$scope.open = function() {
+	console.log("inside loginCtrl method");
+    var modalInstanceVar = $uibModal.open({
+      animation: true,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+	  scope: $scope,
+	  resolve: {}
+    });
+
+    modalInstanceVar.result.then(function(e) {
+     console.log("modal opened")
+    });
+
+  };
+
+
+
+}]).controller('ModalInstanceCtrl',['$rootScope','$scope','$http','$uibModalInstance', function($rootScope,$scope,$http,$uibModalInstance) {
+
+
+
+  $scope.cancel = function() {
+    $uibModalInstance.dismiss('cancel');
+  };
+
+   $scope.closeModal = function () {
+    $uibModalInstance.close();
+  };
+
+  $scope.login = function(e){
+		e.preventDefault();
+		console.log("inside scope.login function");
+		$http({
+			method: "POST",
+			url : $rootScope.webAddr+"/api/v1.0/authenticate",
+			data : angular.toJson($scope.user),
+			header : {
+				'Content-Type' : 'application/json'
+			}
+		}).then(function(response){
+			$rootScope.user = response.data.obj;
+			console.log($rootScope.user);
+			console.log(response);	
+			$rootScope.isLogged = true;
+			$rootScope.user.username = response.data.obj.firstName+" "+response.data.obj.lastName;
+			$rootScope.user.token = response.data.token;
+			sessionStorage.setItem("user",angular.toJson($rootScope.user));
+			console.log($rootScope.user);
+			$scope.resp = "login successfull";
+
+			$uibModalInstance.close();
+		});
+	};
+}]).controller('registerCtrl',['$scope','$http','$uibModal',function($scope,$http,$uibModal){
+
+	var regModalInstanceVar= "";
+
+	$scope.openRegisterModal = function() {
+	console.log("inside loginCtrl method");
+    regModalInstanceVar = $uibModal.open({
+      animation: true,
+      templateUrl: 'myRegisterModalContent.html',
+      controller: 'RegisterModalInstanceCtrl',
+	  scope: $scope,
+	  resolve: {}
+    });
+
+    regModalInstanceVar.result.then(function(e) {
+     console.log("modal opened")
+    });
+
+	console.log(regModalInstanceVar);
+
+  };
+
+  $scope.openLoginModal = function(){
+	  regModalInstanceVar.close();
+	  var loginModalInstance = $uibModal.open({
+		  animation: true,
+		templateUrl: 'myModalContent.html',
+		controller: 'ModalInstanceCtrl'
+	  })
+  }
+}]).controller('RegisterModalInstanceCtrl',['$rootScope','$scope','$http','$uibModalInstance', function($rootScope,$scope,$http,$uibModalInstance) {
+
+  $scope.register = function(e){
+		e.preventDefault();
+		console.log("inside scope.register function");
+		console.log(angular.toJson($scope.user));
+		$scope.user.host = $rootScope.webAddr;
+		$http({
+			method: "POST",
+			url : $rootScope.webAddr+"/api/v1.0/register",
+			data : angular.toJson($scope.user),
+			header : {
+				'Content-Type' : 'application/json'
+			}
+		}).then(function(response){
+			console.log(response);
+			$scope.message = "registration successfull";
+			
+		});
+	};
+}]).controller('logoutCtrl',['$rootScope','$scope',function($rootScope,$scope){
+
+	$scope.logout = function(e){
+		e.preventDefault();
+		console.log("inside logout controller");
+		sessionStorage.removeItem("user");
+		$rootScope.user = {};
+		$rootScope.isLogged = false;
+	}
+
 }]);
